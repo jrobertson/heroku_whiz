b473522a9d4cb8f5e45486ef440b539fcbfa2451
James Robertson <digital.robertson@gmail.com>
2022-03-09 13:43:07 +0000

    feature: Sinatra template now implemented.

--- lib/heroku_whiz.rb
+++ lib/heroku_whiz.rb
@@ -3,6 +3,8 @@
 # file: heroku_whiz.rb
 
 # created: 8th March 2022
+# updated: 9th March 2022
+
 # description: Handy (experimental) Heroku gem for noobs to create a
 #              simple Heroku app in a whiz!
 
@@ -14,7 +16,7 @@
 require 'open-uri'
 require 'fileutils'
 require 'clipboard'
-
+require 'launchy'
 
 # Resources
 #
@@ -23,7 +25,8 @@ require 'clipboard'
 
 # Example usage:
 #
-# hw = HerokuWhiz.new dir: '/home/james/heroku', template: 'rack', appname: 'hello2', debug: true
+# hw = HerokuWhiz.new dir: '/home/james/heroku', template: 'rack',
+#                     appname: 'hello2', debug: true
 #
 # hw.wipe_clean    # removes the previous local app file directory
 # hw.create        # creates the local app file directory
@@ -33,52 +36,44 @@ require 'clipboard'
 # hw.app_url       #=> e.g. https://frozen-dusk-65820.herokuapp.com/
 
 
-class HerokuWhiz
 
-  def initialize(dir: '.', template: 'rack', appname: 'myapp',
-        verbose: true, debug: false)
+class WhizBase
 
-    @dir, @template, @appname, @verbose = dir, template, appname, verbose
+  def initialize(app_path, debug: false)
 
-    @app_path = File.join(@dir, @appname)
-
-
-  end
-
-  def app_url()
-
-    app = `heroku config`.lines.first.split[1]
-    s = "https://#{app}.herokuapp.com/"
-
-    Clipboard.copy s
-    puts 'app URL copied to clipboard.'
-
-    return s
+    @app_path, debug = app_path, debug
 
   end
 
   def create()
 
-    case @template.to_sym
-    when :rack
-      create_rack()
-    else
-      return puts 'template not recognised!'
-    end
+    config = %q(
+run lambda {|env|
+  [200, {'Content-Type'=>'text/plain'}, StringIO.new("Hello World!\n")]
+})
 
-    # build
-    `bundle install`
-    sleep 1
+    gemfile = %q(
+source 'https://rubygems.org'
+gem 'rack'
+gem 'puma'
+)
 
+    # Procfile: see https://devcenter.heroku.com/articles/procfile
+    #           for more info
+    procfile = 'web: bundle exec rackup config.ru -p $PORT'
+
+    create_basefiles(config, gemfile, procfile)
   end
 
+  # currently only removes a file directory if there are no user created files
+  #
   def wipe_clean(setup=:local)
 
     return unless File.exists? @app_path
 
     # remove the app files
     #
-    %w(Gemfile config.ru Gemfile.lock).each do |file|
+    %w(Gemfile config.ru Procfile Gemfile.lock).each do |file|
 
       puts 'removing file ' + file if @debug
       rm File.join(@app_path, file)
@@ -92,8 +87,7 @@ class HerokuWhiz
 
     return unless setup == :both
 
-    app = `heroku config`.lines.first.split[1]
-    `heroku apps:destroy --confirm #{app}`
+    `heroku apps:destroy --confirm #{heroku_app()}`
 
   end
 
@@ -122,12 +116,18 @@ class HerokuWhiz
   end
 
   def local_run()
+
+    #`heroku local &`
+    # attempted to use heroku local but couldn't automatically kill the
+    # process directly
+
     `bundle exec rackup -p 9292 config.ru &`
   end
 
   def local_testrun()
 
-    r = IO.popen( "bundle exec rackup -p 9292 config.ru" )
+    #r = IO.popen("heroku local")
+    r = IO.popen("bundle exec rackup -p 9292 config.ru")
     puts 'r: ' + r.inspect if @debug
     sleep 2
 
@@ -136,36 +136,37 @@ class HerokuWhiz
 
     Process.kill('QUIT', r.pid)
 
-
     puts 'SUCCESS! Ready to deploy' if s == "Hello World!\n"
 
   end
 
   private
 
-  def create_rack()
+  def create_basefiles(config, gemfile, procfile)
 
     FileUtils.mkdir_p @app_path
     FileUtils.chdir @app_path
 
     # write the config.ru file
     #
-    config = %q(
-run lambda {|env|
-  [200, {'Content-Type'=>'text/plain'}, StringIO.new("Hello World!\n")]
-})
     File.write File.join(@app_path, 'config.ru'), config
+    sleep 0.2
 
     # write the Gemfile
     #
-    gemfile = %q(
-source 'https://rubygems.org'
-gem 'rack'
-gem 'puma'
-)
     File.write File.join(@app_path, 'Gemfile'), gemfile
-    sleep 0.5
+    sleep 0.2
 
+    # write the Procfile (although not essential it will flag a warning when
+    # deploying if the file doesn't exist
+    #
+    File.write File.join(@app_path, 'Procfile'), procfile
+    sleep 0.2
+
+  end
+
+  def heroku_app()
+    `heroku config`.lines.first.split[1]
   end
 
   def rm(file)
@@ -182,3 +183,137 @@ gem 'puma'
 
 end
 
+class SinatraWhiz < WhizBase
+
+  def initialize(app_path, debug: false)
+    super(app_path, debug: debug)
+  end
+
+  def create()
+
+    config = %q(
+require './hello'
+run Sinatra::Application
+)
+
+    gemfile = %q(
+source 'https://rubygems.org'
+gem 'sinatra'
+gem 'puma'
+)
+
+    procfile = 'web: bundle exec rackup config.ru -p $PORT'
+
+    create_basefiles(config, gemfile, procfile)
+
+    hello_rb = %q(
+require 'sinatra'
+
+get '/' do
+  "Hello World!"
+end
+)
+    File.write File.join(@app_path, 'hello.rb'), hello_rb
+    sleep 0.3
+
+  end
+
+end
+
+class HerokuWhiz
+
+  def initialize(dir: '.', template: 'rack', appname: 'myapp',
+        verbose: true, debug: false)
+
+    @dir, @template, @appname, @verbose = dir, template, appname, verbose
+
+    @app_path = File.join(@dir, @appname)
+
+    case @template.to_sym
+    when :rack
+      @tapp = WhizBase.new @app_path, debug: debug
+    when :sinatra
+      @tapp = SinatraWhiz.new @app_path, debug: debug
+    end
+
+  end
+
+  def app_url()
+
+    s = "https://#{heroku_app()}.herokuapp.com/"
+
+    Clipboard.copy s
+    puts 'app URL copied to clipboard.'
+
+    return s
+
+  end
+
+  def app_open()
+    `heroku open`
+  end
+
+  def create()
+
+    return unless @tapp
+
+    @tapp.create
+
+    # build
+    `bundle install`
+    sleep 1
+
+  end
+
+  def goto(target)
+
+    case target.to_sym
+    when :apps
+      Launchy.open("https://dashboard.heroku.com/apps")
+    when :docs
+      Launchy.open('https://devcenter.heroku.com/articles/rack')
+    end
+
+  end
+
+  # currently only removes a file directory if there are no user created files
+  #
+  def wipe_clean(setup=:local)
+
+    return unless @tapp
+
+    @tapp.wipe_clean
+
+  end
+
+  def deploy()
+
+    return unless @tapp
+
+    @tapp.deploy
+
+  end
+
+  def local_run()
+
+    return unless @tapp
+
+    @tapp.local_run
+
+  end
+
+  def local_testrun()
+
+    return unless @tapp
+
+    @tapp.local_testrun()
+
+  end
+
+  private
+
+  def heroku_app()
+    `heroku config`.lines.first.split[1]
+  end
+
+end
